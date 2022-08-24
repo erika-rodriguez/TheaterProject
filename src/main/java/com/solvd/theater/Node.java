40f2e0c617e140a93ae34fda6b3e3774b06fbbf3
Erika Rodriguez <92339812+erika-rodriguez@users.noreply.github.com>
2022-08-24 18:16:43 -0300

    Custom LinkedList

---
+++ src/main/java/com/solvd/theater/Node.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solvd.theater;
+
+/**
+ *
+ * @author Erika92
+ */
+import java.io.*;
+
+// Helper Class (Generic node class for LinkedList)
+public class Node<T> {
+    T data;//Storing value of node
+    Node<T> next;//Storing address of next node
+    
+    //Parameterized constructor to assign value
+    public Node(T data) {
+        this.data = data;
+        this.next=null;
+    }
+    
+}
